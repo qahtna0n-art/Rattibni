f2417507bc0170c295e8ce88d54f1f744e6f5dd4
qahtna0n-art <qahtna0n@gmail.com>
2026-04-23 03:28:07 +0300

    Update payment-link.js

--- api/payment-link.js
+++ api/payment-link.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -10,6 +10,7 @@ export default async function handler(req, res) {
 
   try {
     const { amount, description, methods } = req.body;
+
     const moyasarRes = await fetch("https://api.moyasar.com/v1/payment_links", {
       method: "POST",
       headers: {
@@ -24,9 +25,10 @@ export default async function handler(req, res) {
         methods,
       }),
     });
+
     const data = await moyasarRes.json();
     return res.status(moyasarRes.status).json(data);
   } catch (err) {
-    return res.status(500).json({ error: "Server error", details: err.message });
+    return res.status(500).json({ error: err.message });
   }
-}
+};
